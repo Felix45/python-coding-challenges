76c64cfbc144e1818b1d0b43f33d1842bbdc1771
Felix Ouma <fatonoh@gmail.com>
2022-12-08 01:42:33 +0300

    Remove parentheses from condition statement

--- anagrams.py
+++ anagrams.py
@@ -6,7 +6,7 @@ def are_anagrams_two(str_one, str_two):
   str_one = str_one.strip().lower()
   str_two = str_two.strip().lower()
 
-  if(len(str_one) != len(str_two)):
+  if len(str_one) != len(str_two):
     return False
 
   for letter in str_one:
