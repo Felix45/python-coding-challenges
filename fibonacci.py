825448405b9b94aa8c104a94075ef47a20063f30
Felix Ouma <fatonoh@gmail.com>
2022-12-09 13:08:22 +0300

    Optimize fibonacci solution using memoization

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,14 @@
-def fibonacci(position):
+def fibonacci(position, memo = {}):
   ''' This function returns the nth number in a fibonacci sequence '''
+
+  if position in memo:
+    return memo[position]
+
   if position <= 2:
     return 1
-  return fibonacci(position - 1) + fibonacci(position -2)
+  memo[position] = fibonacci(position - 1, memo) + fibonacci(position -2, memo)
+  return memo[position]
 
-print(fibonacci(6))
-print(fibonacci(7))
-print(fibonacci(8))
+print(fibonacci(40))
+print(fibonacci(50))
+print(fibonacci(60))
