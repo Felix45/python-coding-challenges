4c8329b88ddb51718213a6cfe19f3afb8750ba14
Felix Ouma <fatonoh@gmail.com>
2022-12-11 18:35:22 +0300

    Optimize can sum coding challenge solution using memoization

--- cansum/can_sum.py
+++ cansum/can_sum.py
@@ -1,6 +1,8 @@
-def can_sum(target_sum, arr):
+def can_sum(target_sum, arr, memo={}):
   ''' Returns boolean (true) if the elements of an array can sum up to target sum '''
-  
+  if target_sum in memo:
+    return memo[target_sum]
+
   if target_sum == 0:
     return True
 
@@ -9,7 +11,9 @@ def can_sum(target_sum, arr):
 
   for number in arr:
     new_target_sum = target_sum - number
-    if(can_sum(new_target_sum, arr) == True):
+    if can_sum(new_target_sum, arr, memo) == True:
+      memo[target_sum] = True
       return True
 
+  memo[target_sum] = False  
   return False   
