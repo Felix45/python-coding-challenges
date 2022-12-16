30e224ac90f1e9b82d357c6dbe922c4f6d36d7a8
Felix Ouma <fatonoh@gmail.com>
2022-12-16 11:52:23 +0300

    Optimize can construct bruteforce solution

--- can-construct/can_construct.py
+++ can-construct/can_construct.py
@@ -1,16 +1,17 @@
-def can_construct(target_string, word_bank):
+def can_construct(target_string, word_bank, memo={}):
   '''
     Returns a boolean indicating whether or not the `target` 
     can be constructed by concatenating elements of the `word_bank` array.
   '''
-
-  if target_string == '':
-    return True
+  if target_string in memo: return memo[target_string]
+  if target_string == '': return True
 
   for word in word_bank:
-    if target_string[0:len(word)] == word:
+    if target_string.startswith(word):
       new_target_str = target_string[len(word):]
       if can_construct(new_target_str, word_bank) == True:
+        memo[target_string] = True
         return True
 
+  memo[target_string] = False
   return False
