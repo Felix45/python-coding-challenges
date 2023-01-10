bc0330db05f868e11ea9af11dcc45ea85d48938d
Felix Ouma <fatonoh@gmail.com>
2023-01-10 23:10:46 +0300

    Update definition of test class

--- strings/challenge-indexof/indexof_test.py
+++ strings/challenge-indexof/indexof_test.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 from indexof import getOccurence
 
-class PalindromeTest(TestCase):
+class GetOccurenceTest(TestCase):
     def test_get_occurence_h(self):
       assert getOccurence('This is a dish', 'h') == [1, 13]
 
