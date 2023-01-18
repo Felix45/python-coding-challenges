2456fa5e327569f65451f1f9eb46049ffa02013a
Felix Ouma <fatonoh@gmail.com>
2023-01-18 15:59:32 +0300

    Update unit tests for median sorted

--- arrays/median-of-sortedarrays/linear_median_sorted_test.py
+++ arrays/median-of-sortedarrays/linear_median_sorted_test.py
@@ -3,10 +3,10 @@ from linear_median_sorted import linear_median_sorted
 
 class MedainSortedArrayTest(TestCase):
     def test_linear_median_sorted_I(self):
-      assert linear_median_sorted([1,3], [2]) == 2
+      assert linear_median_sorted([1,3], [0,2]) == 1.5
 
     def test_linear_median_sorted_II(self):
       assert linear_median_sorted([1,2], [3,4]) == 2.5
 
     def test_linear_median_sorted_III(self):
-      assert linear_median_sorted([1,3,5,6], [-1,2]) == 2.5
+      assert linear_median_sorted([1,3,5,6], [-3, -1, 0, 2]) == 1.5
