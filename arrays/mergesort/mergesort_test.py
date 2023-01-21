9ccf9de4d98df4b859e8cd3eaf101f82fa592bcd
Felix Ouma <fatonoh@gmail.com>
2023-01-21 22:19:25 +0300

    Update mergesort unit tests

--- arrays/mergesort/mergesort_test.py
+++ arrays/mergesort/mergesort_test.py
@@ -3,10 +3,10 @@ from mergesort import mergesort
 
 class MergeSortTest(TestCase):
     def test_mergesort_I(self):
-      assert mergesort([6, 3, 5, 1]) == [1, 3, 5, 6]
+      assert mergesort([6, 3, 5, 1], 4) == [1, 3, 5, 6]
 
     def test_mergesort_II(self):
-      assert mergesort([-1,-3,0,2]) == [-3, -1, 0, 2]
+      assert mergesort([-1,-3,0,2], 4) == [-3, -1, 0, 2]
 
     def test_mergesort_III(self):
-      assert mergesort([-1,4,-5,-6]) == [-6, -5, -1, 4]
+       assert mergesort([-1,4,0,-1,-5,-6], 6) == [-6, -5, -1, -1, 0, 4]
