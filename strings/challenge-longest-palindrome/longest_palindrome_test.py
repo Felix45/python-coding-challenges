39f69045db5f20da953542fdd3e5518f4e29969e
Felix Ouma <fatonoh@gmail.com>
2023-03-26 23:27:53 +0300

    Add unit tests for longest_palindrome function

---
+++ strings/challenge-longest-palindrome/longest_palindrome_test.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+from longest_palindrome import longest_palindrome
+
+class LongestPalindromeTest(TestCase):
+    def test_longest_palindrome_I(self):
+      assert longest_palindrome('babad') == 'bab'
+
+    def test_longest_palindrome_I(self):
+      assert longest_palindrome('cbbd') == "bb"
