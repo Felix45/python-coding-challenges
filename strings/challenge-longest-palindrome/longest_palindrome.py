eaedc5fead758eb5d4b0e5dac8647451888ffc83
Felix Ouma <fatonoh@gmail.com>
2023-03-26 23:46:28 +0300

    Add expand about the center function

--- strings/challenge-longest-palindrome/longest_palindrome.py
+++ strings/challenge-longest-palindrome/longest_palindrome.py
@@ -1,10 +1,17 @@
+def expand(str, begin, end):
+  while begin >= 0 and end <= end and str[begin] == str[end]:
+    begin -= 1
+    end   += 1
+
+  return str[(begin + 1):end]
+
 def longest_palindrome(str):
   if len(str) <= 0:
     return str
   
   longest = str[0:1]
   
-  for i in range(0, len(str)):
+  for i in range(0, len(str) - 1):
     temp = expand(str, i, i)
 
     if len(temp) > len(longest):
