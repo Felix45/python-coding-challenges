7d1d56f2eafdf7a6546acb5bf7dbb6bcc77b9acb
felix45 <fatonoh@gmail.com>
2023-06-18 22:26:51 +0300

    Add unit test for the permutation_sequence function

---
+++ permutation/permutation-sequence/permutation_sequence_test.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+from permutation_sequence import permutation_sequence
+
+class PermutationSequence(TestCase):
+  ''' Unit test for the permutation sequence function '''
+  def test_permutation_sequence_I(self):
+    assert permutation_sequence(3,3) == "213"
+
+  def test_permutation_sequence_II(self):
+    assert permutation_sequence(4,9) == "1234"
+
+  def test_permutation_sequence_III(self):
+    assert permutation_sequence(3,1) == "123"
