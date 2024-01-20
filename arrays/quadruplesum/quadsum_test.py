38392d8f5d6900e77a0f89fb6daa935b1aa0d8c3
felix45 <fatonoh@gmail.com>
2024-01-20 23:49:26 +0300

    Fix failing error

--- arrays/quadruplesum/quadsum_test.py
+++ arrays/quadruplesum/quadsum_test.py
@@ -3,7 +3,7 @@ from quadruplesum import quad_sum
 
 class QuadSumTest(TestCase):
     def test_quad_sum_I(self):
-        assert quad_sum([1, 0, -1, 0, -2, 2], 0) == [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+        assert quad_sum([1, 0, -1, 0, -2, 2], 0) == [[-1,0,0,1],[-2,-1,1,2],[-2,0,0,2]]
     def test_quad_sum_II(self):
         assert quad_sum([2, 2, 2, 2, 2], 8) == [[2, 2, 2, 2]]
 
