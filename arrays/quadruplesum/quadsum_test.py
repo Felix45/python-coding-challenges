9dc00f348c847b146e9713a9520d8160b55d08ae
felix45 <fatonoh@gmail.com>
2024-01-20 23:41:06 +0300

    Add test cases for the quadsum problem

---
+++ arrays/quadruplesum/quadsum_test.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from quadruplesum import quad_sum
+
+class QuadSumTest(TestCase):
+    def test_quad_sum_I(self):
+        assert quad_sum([1, 0, -1, 0, -2, 2], 0) == [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+    def test_quad_sum_II(self):
+        assert quad_sum([2, 2, 2, 2, 2], 8) == [[2, 2, 2, 2]]
+
+    def test_quad_sum_III(self):
+        assert quad_sum([1, 1, 1, 1, 1], 4) == [[1, 1, 1, 1]]
+
+    def test_quad_sum_IV(self):
+        assert quad_sum([1, 1, 1, 1, 1, 1], 4) == [[1, 1, 1, 1]]
